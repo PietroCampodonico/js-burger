7d9807ae0a8dac552de7ef03ff23e19ba0d149ac
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-19 19:55:13 +0200

    aggiunto controllo sconto

---
+++ burger/js/esperimentoCoupon.js
@@ -0,0 +1,18 @@
+//CODICE INCOMPLETO!!!!!!!
+
+// var couponField = document.getElementById("coupon");
+// var couponsValidi = ["asda1566", "gdfg1651", "werw0616", "qweqw6548",]
+
+
+// couponsValidi = false;
+
+// for (var i = 0; i < couponsValidi.length; i++) {
+
+//     if (couponsValidi === couponField.value) {
+//         couponsValidi = true;
+//     }
+// }
+
+
+
+
--- burger/js/script.js
+++ burger/js/script.js
@@ -4,41 +4,54 @@ window.addEventListener("load", function(){
 })
 
 var submitBtn = document.getElementById("button");
-var burgerCustomName = document.getElementsByClassName("name-burger");
-var couponsValidi = ["asda1566", "gdfg1651", "werw0616", "qweqw6548",]
+var burgerCustomName = document.getElementById("name");
 var sum = 50;
+var discountPrice = sum - 20;
+var couponField = document.getElementById("coupon");
+var couponsValidi = ["asda1566", "gdfg1651", "werw0616", "qweqw6548",]
 
 
 function subTotal() {
     submitBtn.addEventListener("click", function(){
         var ingredientsList = document.querySelectorAll("input.ingredient-checkbox");
+        var couponCheck = false;
 
         if (!burgerCustomName.value) {
             alert("inserire il nome dell'hamburger personalizzato!")
-
         } else {
-            for (var i = 0; i < ingredientsList.length; i++) {
-                
-                //FACCIO UN CICLO FOR CON UN IF PER OGNI ELEMENTO DELLA LISTA
-                /*if (ingredientsList[i].checked) {
-                    sum += parseInt(ingredientsList[i].value);
-                }*/
+            for (var i = 0; i < couponsValidi.length; i++) {
     
-             //OPPURE SCRIVO UNA FUNZIONE GENERICA CHE RICHIAMO NEL CICLO
-               
-               sum = addToSum(sum, ingredientsList[i])
-            } 
-        }    
-        
-        console.log(sum)
-    })  
+                if (couponField.value === couponsValidi[i]) {
 
+                    couponCheck = true;
+
+                    alert("Hai diritto a uno sconto!");
+
+                    for (var i = 0; i < ingredientsList.length; i++) {
+                        sum = addToSum(sum, ingredientsList[i]);
+                    }
+
+                    document.getElementById("price").innerText = discountPrice;
+
+                } else {
+
+                    for (var i = 0; i < ingredientsList.length; i++) {
+                        sum = addToSum(sum, ingredientsList[i]);
+                    }
+                    document.getElementById("price").innerText = sum;
+                }
+            }
+        }  
+    })  
 }
 
 function addToSum(sommaPrecedente, elementoLista) {
     if (elementoLista.checked) {
         sommaPrecedente += parseInt(elementoLista.value);
     }
-
+    
     return sommaPrecedente
 }
+
+
+
